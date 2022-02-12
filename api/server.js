5e6d46af5bd7ee784b94582b97586514f6080949
xavioo <rucahoxavi@gmail.com>
2022-02-12 07:42:11 +0200

    run root main get routes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("welcome to the e-shop api...");
+  });
+
+
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, console.log(`server run in port ${PORT}`));
