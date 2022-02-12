feaa5bd2e2bf1c90ddf61eed10c4f1d0388a3a6c
xavioo <rucahoxavi@gmail.com>
2022-02-12 13:36:04 +0200

    comment single product

--- api/server.js
+++ api/server.js
@@ -11,10 +11,29 @@ app.get("/", (req, res) => {
     res.send("welcome to the e-shop api...");
   });
 
+//all products
 app.get("/api/products", (req, res) => {
 res.json(products);
 });
 
+//single product
+// app.get("/api/products/:id", (req, res) => {
+//     const product=products.find((p) => p._id === req.params.id);
+//     console.log(product);
+//     res.json(product);
+//     });
+
+
+// app.get("/api/products/:id",(req, res) => {
+//     const id = Number(req.params.id)
+//     const product = products.find(product => product._id === id)
+//         if (!product) {
+//         return res.status(404).send('Product not found')
+//     }
+//     res.json(product)
+// })    
+
+//all users
 app.get("/api/users", (req, res) => {
     res.json(users);
     });
