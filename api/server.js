1826d3a2c234838634af0146c96c14264100367d
xavioo <rucahoxavi@gmail.com>
2022-02-12 22:33:57 +0200

    login

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ import ImportData from "./DataImport.js";
 import connectDatabase from "./config/MongoDb.js";
 import productRoute from "./Routes/ProductRoutes.js";
 import { errorHandler, notFound } from "./Middleware/Errors.js";
+import userRouter from "./Routes/UserRoutes.js";
 
 dotenv.config();
 connectDatabase();
@@ -12,10 +13,12 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("welcome to the e-shop api...");
-  });
+ });
+
 // API
 app.use("/api/import", ImportData);  
 app.use("/api/products", productRoute);
+app.use("/api/users", userRouter);
 
 
 // ERROR HANDLER
